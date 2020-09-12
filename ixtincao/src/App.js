571726270884f135ee110a7f724cdacfb7288b60
VictorRIZ <victorari856@gmail.com>
2020-09-12 19:15:46 -0300

    primeiro commit Victor

--- ixtincao/src/App.js
+++ ixtincao/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var timeA = 1
+var timeB = 2
+var nometimeA ="São Paulo"
+var nometimeB = "Cruzeiro"
+
 function App() {
   return (
     <div className="App">
@@ -9,7 +14,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {`Time ${nometimeA} Fez ${timeA} gols contra o ${nometimeB} que fez ${timeB} gols `}
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
